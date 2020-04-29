988598583063213ed176b3f2c2c625e08c9a00f5
Laila <sprejerlaila@gmail.com>
2020-04-29 18:49:10 +0100

    adding streaming for seed nodes

---
+++ stream_seeds.py
@@ -0,0 +1,51 @@
+import tweepy
+import sys
+import json
+import time
+from accessPoints_Sprejer import TwitterAuth50 as auth
+
+with open('seed_ids.txt') as file:
+    ids = file.read().splitlines()
+
+with open('seed_users.txt') as file:
+    users = file.read().splitlines()
+    
+oauth = tweepy.OAuthHandler(auth.consumer_key, auth.consumer_secret)
+oauth.set_access_token(auth.access_token, auth.access_token_secret)
+api = tweepy.API(oauth)
+
+class StreamListener(tweepy.StreamListener):
+    def __init__(self, output_file=sys.stdout):
+        super(StreamListener,self).__init__()
+        self.output_file = output_file
+        
+    def on_status(self, status):
+        #with open(self.output_file, 'a') as tf:
+        with open('data/seed_tweets/seed_tweets_{}.json'.format(time.strftime("%y%m%d")), 'a') as tf:
+            # Write a new line
+            tf.write('\n')
+
+            # Write the json data directly to the file
+            json.dump(status._json, tf)
+            # Alternatively: tf.write(json.dumps(all_data))
+        
+        print(status.text)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+
+if __name__ == "__main__":
+    while True:
+        stream_listener = StreamListener()
+        stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+        try:
+            print("streaming...")
+            stream.filter(follow=ids, track=users)
+        except Exception:
+            print("error. Restarting Stream... Error:")
+            time.sleep(60)
+
+
+
