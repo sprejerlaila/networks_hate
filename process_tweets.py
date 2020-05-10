21ba84f4f24784a69a1074fa40aed5baecc14bfe
Laila <sprejerlaila@gmail.com>
2020-05-10 17:52:18 +0100

    merging stream and rest tweets

--- process_tweets.py
+++ process_tweets.py
@@ -39,14 +39,14 @@ def tidy_tweets(raw_tweets):
         retweet_count.append(tweet['retweet_count']) #TODO replace with rt fav count when available
         favorite_count.append(tweet['favorite_count']) #TODO replace with rt fav count when available
         
-        text = tweet['text']
+        text = tweet['text'] if "text" in tweet else tweet['full_text']
         
         rt_screen_name, rt_id, qt_id, qt_screen_name, qt_status = None, None, None, None, None
         
         mentions = set([x['screen_name'] for x in tweet['entities']['user_mentions']])
         
         if 'retweeted_status' in tweet: # It is a retweet 
-            text = tweet['retweeted_status']['text']
+            text = tweet['retweeted_status']['text'] if 'text' in tweet['retweeted_status'] else tweet['retweeted_status']['full_text']
             try:
                 text = tweet['retweeted_status']['extended_tweet']['full_text']
                 mentions = set([x['screen_name'] for x in tweet['retweeted_status']['extended_tweet']['entities']['user_mentions']])
@@ -58,7 +58,7 @@ def tidy_tweets(raw_tweets):
         if 'quoted_status' in tweet: # In reply to tweet data
             qt_id = tweet["quoted_status"]["user"]["id"]
             qt_screen_name = tweet["quoted_status"]["user"]["screen_name"]
-            qt_status = tweet["quoted_status"]["text"]
+            qt_status = tweet["quoted_status"]["text"] if 'text' in tweet['quoted_status'] else tweet['quoted_status']['full_text']
             try:
                 qt_status = tweet["quoted_status"]["extended_tweet"]['full_text']
 
@@ -99,15 +99,21 @@ def tidy_tweets(raw_tweets):
     
     return data
 
-with open('seed_users.txt') as file:
-    users = file.read().splitlines()
+users = list(pd.read_csv("seed_users.csv").user.values)
 
 class process_tweets():
     def __init__(self, day_to_process):
         self.day_to_process = day_to_process
         with open('data/seed_tweets/seed_tweets_{}.json'.format(day_to_process)) as json_data:
-            tweets = json_data.readlines()
-        self.tweets = tidy_tweets(tweets)
+            stream_tweets = json_data.readlines()
+        stream_tweets = tidy_tweets(stream_tweets)
+        
+        with open('data/seed_tweets/rest_tweets_{}.json'.format(day_to_process)) as json_data:
+            rest_tweets = json_data.readlines()
+        rest_tweets = tidy_tweets(rest_tweets)
+
+        
+        self.tweets = pd.concat([stream_tweets, rest_tweets[~rest_tweets.id.isin(stream_tweets.id)]]).reset_index(drop=True)
         self.extract_seed_tweets()
         self.extract_seed_retweets()
         
