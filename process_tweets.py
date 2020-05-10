8d58aa651030ae1cdac58ea5c0f3fa6da7511624
Laila <sprejerlaila@gmail.com>
2020-05-10 14:37:04 +0100

    process raw collected tweets. separate seed and retweeted. create seed id list

--- process_tweets.py
+++ process_tweets.py
@@ -2,15 +2,15 @@ import pandas as pd
 import json
 import time
 
-
-def structure_results(results):
+    
+def tidy_tweets(raw_tweets):
     id_list = []
     screen_name_list = []
 
     text_list = []
     in_reply_to_user_id, in_reply_to_status, in_reply_to_screen_name = [], [], []
     
-    rt_from_screen_name = []
+    rt_screen_name_list, rt_id_list = [], []
     qt_screen_name_list, qt_id_list, qt_status_list  = [], [], []
     
     mentions_list = []
@@ -21,7 +21,7 @@ def structure_results(results):
     retweet_count = []
     favorite_count = []
 
-    for idx, tweet in enumerate(results):       
+    for idx, tweet in enumerate(raw_tweets):       
         tweet = json.loads(tweet)
         id_list.append(tweet['id'])
         
@@ -41,7 +41,7 @@ def structure_results(results):
         
         text = tweet['text']
         
-        rt_screen_name, qt_id, qt_screen_name, qt_status = None, None, None, None
+        rt_screen_name, rt_id, qt_id, qt_screen_name, qt_status = None, None, None, None, None
         
         mentions = set([x['screen_name'] for x in tweet['entities']['user_mentions']])
         
@@ -53,15 +53,20 @@ def structure_results(results):
             except:
                 pass
             rt_screen_name = tweet['retweeted_status']['user']['screen_name']
+            rt_id = tweet['retweeted_status']['id']
 
         if 'quoted_status' in tweet: # In reply to tweet data
             qt_id = tweet["quoted_status"]["user"]["id"]
             qt_screen_name = tweet["quoted_status"]["user"]["screen_name"]
             qt_status = tweet["quoted_status"]["text"]
             try:
-                qt_status = tweet["quoted_status"]["user"]["text"]
+                qt_status = tweet["quoted_status"]["extended_tweet"]['full_text']
+
             except:
-                pass
+                try:
+                    qt_status = tweet["quoted_status"]["user"]["text"]
+                except:
+                    pass
             
         if 'extended_tweet' in tweet:
             text = tweet['extended_tweet']['full_text']
@@ -71,7 +76,8 @@ def structure_results(results):
         in_reply_to_status.append(rp_status)
         in_reply_to_user_id.append(rp_user_id)
         in_reply_to_screen_name.append(rp_screen_name)
-        rt_from_screen_name.append(rt_screen_name)
+        rt_screen_name_list.append(rt_screen_name)
+        rt_id_list.append(rt_id)
         qt_id_list.append(qt_id)
         qt_screen_name_list.append(qt_screen_name)
         qt_status_list.append(qt_status)
@@ -82,11 +88,53 @@ def structure_results(results):
         "id" : id_list,
         "screen_name": screen_name_list,
         "text": text_list,
-        "rt_from_screen_name": rt_from_screen_name,
+        "rt_from_screen_name": rt_screen_name_list,
+        "rt_from_id": rt_id_list,
         "qt_from_screen_name": qt_screen_name_list,
         'qt_status': qt_status_list,
         "in_reply_to_screen_name": in_reply_to_screen_name,
         "in_reply_to_status": in_reply_to_status,
         "mentions": mentions_list,
         "datetime":datetime})
+    
     return data
+
+with open('seed_users.txt') as file:
+    users = file.read().splitlines()
+
+class process_tweets():
+    def __init__(self, day_to_process):
+        self.day_to_process = day_to_process
+        with open('data/seed_tweets/seed_tweets_{}.json'.format(day_to_process)) as json_data:
+            tweets = json_data.readlines()
+        self.tweets = tidy_tweets(tweets)
+        self.extract_seed_tweets()
+        self.extract_seed_retweets()
+        
+    
+        
+    def extract_seed_tweets(self):
+        print("Extracting seed tweets, saving to data/processed/seed_tweets/")
+        seed_tweets = self.tweets[self.tweets.screen_name.isin(users)]
+        
+        # Write full dataframe
+        seed_tweets.to_csv("data/processed/seed_tweets/processed_seed_tweets_{}.csv".format(self.day_to_process), index=False)
+        
+        # Keep a list of seed tweets ids
+        with open('data/processed/seed_tweets/seed_tweets_ids.csv', 'a') as f: 
+            for tweet_id in seed_tweets.id.values:
+                    f.write("%s\n" % tweet_id)
+        
+    def extract_seed_retweets(self):
+        print("Extracting seed retweets, saving to data/processed/seed_retweets/")
+        with open('data/processed/seed_tweets/seed_tweets_ids.csv') as file:
+            ids = file.read().splitlines()
+        
+        seed_reweets = self.tweets[self.tweets.rt_from_id.isin(ids)]
+        seed_reweets.to_csv("data/processed/seed_retweets/processed_seed_retweets_{}.csv".format(self.day_to_process), index=False)
+    
+    
+    
+    
+    
+    
