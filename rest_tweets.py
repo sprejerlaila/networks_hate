a4353099e883c929f1daf63a2f5a45b533a22398
Laila <sprejerlaila@gmail.com>
2020-05-14 14:25:37 +0100

    request by user_id. add run from main

--- rest_tweets.py
+++ rest_tweets.py
@@ -1,4 +1,5 @@
 import time
+import datetime as dt
 import pandas as pd
 import requests
 from accessPoints_Sprejer import TwitterAuth52 as auth
@@ -19,36 +20,37 @@ class get_tweets():
         self.since = time.strptime(since, '%Y-%m-%d')
         self.until = until
         
-        users = list(pd.read_csv("seed_users.csv").user.values)
-        users = ["@"+user for user in users]
-        for user in users:
-            self.get_timeline(user)
-            self.get_mentions(user)
+        users = pd.read_csv("data/seed_users.csv")
+        users['screen_name'] = '@' + users['screen_name'].astype(str)
+        
+        for idx, row in users.iterrows():
+            self.get_timeline(row['user_id'])
+            self.get_mentions(row['screen_name'])
         
 
         
-    def get_timeline(self, user):
+    def get_timeline(self, user_id):
         #TODO add pagination!!! 
         until = time.strptime(self.until, '%Y-%m-%d')
         response = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json",
-                        params = {"screen_name": user, "tweet_mode": "extended"},
+                        params = {"user_id": user_id, "tweet_mode": "extended"},
                         auth=oauth)
         
         for tweet in response.json():
             tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
             if tw_date > self.since and tw_date < until:
-                with open('data/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d", self.since)), 'a') as tf:
+                with open('data/raw/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d", self.since)), 'a') as tf:
                 
                     # Write the json data directly to the file
                     json.dump(tweet, tf)
                     
                     tf.write('\n')
 
-    def get_mentions(self, user):
+    def get_mentions(self, screen_name):
         next_page_url = "https://api.twitter.com/1.1/search/tweets.json"
         while True:
             response = requests.get(next_page_url,
-                                params = {"q": user, "count":200, "until":self.until, "tweet_mode": "extended"},
+                                params = {"q": screen_name, "count":200, "until":self.until, "tweet_mode": "extended"},
                                 auth=oauth)
 
             response = response.json()
@@ -56,7 +58,7 @@ class get_tweets():
             for tweet in response['statuses']:
                 tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
                 if tw_date > self.since:
-                    with open('data/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d",self.since)), 'a') as tf:
+                    with open('data/raw/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d",self.since)), 'a') as tf:
                     
                         # Write the json data directly to the file
                         json.dump(tweet, tf)
@@ -67,3 +69,8 @@ class get_tweets():
                 break    
             next_page_url = "https://api.twitter.com/1.1/search/tweets.json" \
             + response['search_metadata']['next_results']
+
+if __name__ == "__main__":
+    yesterday = dt.datetime.strftime(dt.datetime.now() - dt.timedelta(1), '%Y-%m-%d')
+    today = time.strftime("%Y-%m-%d")
+    get_tweets(since=yesterday, until=today)
