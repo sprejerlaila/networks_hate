ae8da8f3fce73943fb61373c571cccf44abd8a30
Laila <sprejerlaila@gmail.com>
2020-05-16 15:37:38 +0100

    handling rate limits

--- rest_tweets.py
+++ rest_tweets.py
@@ -23,59 +23,87 @@ class get_tweets():
         users = pd.read_csv("data/seed_users.csv")
         users['screen_name'] = '@' + users['screen_name'].astype(str)
         
-        for idx, row in users.iterrows():
-            self.get_timeline(row['user_id'])
-            self.get_mentions(row['screen_name'])
+        print('getting timeline')
+        self.get_timeline(users['user_id'].values)
+        
+        print('getting mentions')
+        self.get_mentions(users['screen_name'].values)
         
 
         
-    def get_timeline(self, user_id):
+    def get_timeline(self, users):
         until = time.strptime(self.until, '%Y-%m-%d')
-        max_id = None
-        while True:
-            response = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json",
-                            params = {"user_id": user_id, "tweet_mode": "extended", "count":200, "max_id":max_id},
-                            auth=oauth)
-            
-            for tweet in response.json():
-                tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
-                if tw_date > self.since and tw_date < until:
-                    with open('data/raw/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d", self.since)), 'a') as tf:
-                    
-                        # Write the json data directly to the file
-                        json.dump(tweet, tf)
-                        
-                        tf.write('\n')
-            if tw_date < self.since:
-                break
-            else:
-                max_id = tweet['id']
-
-    def get_mentions(self, screen_name):
-        next_page_url = "https://api.twitter.com/1.1/search/tweets.json"
-        while True:
-            response = requests.get(next_page_url,
-                                params = {"q": screen_name, "count":200, "until":self.until, "tweet_mode": "extended"},
+        times = [] # Control rate limit.       
+        
+        for user_id in users:
+            max_id = None
+            while True:
+                if len(times) == 900: # if 900 requests were already done
+                    if times[-1] - times[0] < 900: # check not exceding the rate limit (15 min window)
+                        print("waiting the needed time before continuing")
+                        time.sleep(901-(times[-1] - times[0])) # wait the rest of the 15 minutes
+                    times = times[1:] # remove the initial time 
+                            
+                times.append(time.time()) # adding time of the new request
+                response = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json",
+                                params = {"user_id": user_id, "tweet_mode": "extended", "count":200, "max_id":max_id},
                                 auth=oauth)
-
-            response = response.json()
-            
-            for tweet in response['statuses']:
-                tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
-                if tw_date > self.since:
-                    with open('data/raw/seed_tweets/rest_tweets_{}.json'.format(time.strftime("%y%m%d",self.since)), 'a') as tf:
-                    
-                        # Write the json data directly to the file
-                        json.dump(tweet, tf)
+                
+                for tweet in response.json():
+                    tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+                    if tw_date > self.since and tw_date < until:
+                        with open('data/raw/seed_tweets/test_rest_tweets_{}.json'.format(time.strftime("%y%m%d", self.since)), 'a') as tf:
                         
-                        tf.write('\n')
+                            # Write the json data directly to the file
+                            json.dump(tweet, tf)
+                            
+                            tf.write('\n')
+                
+                if tw_date < self.since: # tw_date is the date of the last tweet collected
+                    break
+                else:
+                    max_id = tweet['id']
+
+    def get_mentions(self, users):
+        next_page_url = "https://api.twitter.com/1.1/search/tweets.json"
+        times = []
+        
+        for screen_name in users:
             
-            if tw_date < self.since:
-                break    
-            next_page_url = "https://api.twitter.com/1.1/search/tweets.json" \
-            + response['search_metadata']['next_results']
+            while True:
+                if len(times) == 180: # if 180 requests were already done
+                    if times[-1] - times[0] < 900: # check not exceding the rate limit (15 min window)
+                        print("waiting the needed time before continuing")
+                        time.sleep(901-(times[-1] - times[0])) # wait the rest of the 15 minutes
+                    times = times[1:] # remove the initial time 
+                            
+                times.append(time.time()) # adding time of the new request
+                
+                response = requests.get(next_page_url,
+                                    params = {"q": screen_name, "count":200, "until":self.until, "tweet_mode": "extended"},
+                                    auth=oauth)
+    
+                response = response.json()
+                
+                for tweet in response['statuses']:
+                    tw_date = time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+                    if tw_date > self.since:
+                        with open('data/raw/seed_tweets/test_rest_tweets_{}.json'.format(time.strftime("%y%m%d",self.since)), 'a') as tf:
+                        
+                            # Write the json data directly to the file
+                            json.dump(tweet, tf)
+                            
+                            tf.write('\n')
+                
+                if tw_date < self.since:
+                    break    
+                next_page_url = "https://api.twitter.com/1.1/search/tweets.json" \
+                + response['search_metadata']['next_results']
 
 if __name__ == "__main__":
     yesterday = dt.datetime.strftime(dt.datetime.now() - dt.timedelta(1), '%Y-%m-%d')
     today = time.strftime("%Y-%m-%d")
     get_tweets(since=yesterday, until=today)
+    
+    
+    
