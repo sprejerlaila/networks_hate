0c8fad73676bb3bf60e9f73785ded3a9e57e7338
Laila <sprejerlaila@gmail.com>
2020-06-28 08:57:21 +0100

    add access points. run daily

--- get_followers.py
+++ get_followers.py
@@ -5,11 +5,22 @@ import time
 import pandas as pd
 import requests
 from requests_oauthlib import OAuth1
-from accessPoints_Sprejer import TwitterAuth51 as auth_seed
+from accessPoints_Sprejer import TwitterAuth53 as auth_seed
 from accessPoints_Sprejer import TwitterAuth41 as auth0
 from accessPoints_Sprejer import TwitterAuth42 as auth1
 from accessPoints_Sprejer import TwitterAuth43 as auth2
-
+from accessPoints_Sprejer import TwitterAuth44 as auth3
+from accessPoints_Sprejer import TwitterAuth45 as auth4
+from accessPoints_Sprejer import TwitterAuth138 as auth5
+from accessPoints_Sprejer import TwitterAuth139 as auth6
+from accessPoints_Sprejer import TwitterAuth140 as auth7
+from accessPoints_Sprejer import TwitterAuth141 as auth8
+from accessPoints_Sprejer import TwitterAuth142 as auth9
+from accessPoints_Sprejer import TwitterAuth143 as auth10
+from accessPoints_Sprejer import TwitterAuth144 as auth11
+from accessPoints_Sprejer import TwitterAuth145 as auth12
+from accessPoints_Sprejer import TwitterAuth146 as auth13
+from accessPoints_Sprejer import TwitterAuth147 as auth14
 
 oauth_seed = OAuth1(auth_seed.consumer_key, auth_seed.consumer_secret, auth_seed.access_token, auth_seed.access_token_secret)
 
@@ -19,7 +30,32 @@ oauth1 = OAuth1(auth1.consumer_key, auth1.consumer_secret, auth1.access_token, a
 
 oauth2 = OAuth1(auth2.consumer_key, auth2.consumer_secret, auth2.access_token, auth2.access_token_secret)
 
-oauths = [oauth0, oauth1, oauth2]
+oauth3 = OAuth1(auth3.consumer_key, auth3.consumer_secret, auth3.access_token, auth3.access_token_secret)
+
+oauth4 = OAuth1(auth4.consumer_key, auth4.consumer_secret, auth4.access_token, auth4.access_token_secret)
+
+oauth5 = OAuth1(auth5.consumer_key, auth5.consumer_secret, auth5.access_token, auth5.access_token_secret)
+
+oauth6 = OAuth1(auth6.consumer_key, auth6.consumer_secret, auth6.access_token, auth6.access_token_secret)
+
+oauth7 = OAuth1(auth7.consumer_key, auth7.consumer_secret, auth7.access_token, auth7.access_token_secret)
+
+oauth8 = OAuth1(auth8.consumer_key, auth8.consumer_secret, auth8.access_token, auth8.access_token_secret)
+
+oauth9 = OAuth1(auth9.consumer_key, auth9.consumer_secret, auth9.access_token, auth9.access_token_secret)
+
+oauth10 = OAuth1(auth10.consumer_key, auth10.consumer_secret, auth10.access_token, auth10.access_token_secret)
+
+oauth11 = OAuth1(auth11.consumer_key, auth11.consumer_secret, auth11.access_token, auth11.access_token_secret)
+
+oauth12 = OAuth1(auth12.consumer_key, auth12.consumer_secret, auth12.access_token, auth12.access_token_secret)
+
+oauth13 = OAuth1(auth13.consumer_key, auth13.consumer_secret, auth13.access_token, auth13.access_token_secret)
+
+oauth14 = OAuth1(auth14.consumer_key, auth14.consumer_secret, auth14.access_token, auth14.access_token_secret)
+
+oauths = [oauth0, oauth1, oauth2, oauth3, oauth4, oauth5, oauth6, oauth7, oauth8, oauth9, oauth10, oauth11, oauth12, oauth13, oauth14]
+
 
 def get_followers(user_id_list, n_seeds, oauth=oauth0, n_group = 0, datetime=time.strftime("%y%m%d")):
     times = [] # Control not exceeding the rate limit
@@ -45,17 +81,17 @@ def get_followers(user_id_list, n_seeds, oauth=oauth0, n_group = 0, datetime=tim
                 with open('data/processed/{}_followers/{}_followers_{}_{}.csv'.format(user_type, user_type, datetime,n_group), 'a') as f: 
                     for follower_id in response.json()['ids']:
                         f.write(str(user_id) + "," + str(follower_id) + "\n")
+                if cursor == -1:
+                    with open('data/collected_followers_ids.csv', 'a') as f:
+                        f.write("%s\n" % user_id)
             except:
                 print(user_id, response.json())
                 break
             cursor = response.json()['next_cursor']
         
-        with open('data/collected_followers_ids.csv', 'a') as f: 
-            f.write("%s\n" % user_id)
-            
 if __name__ == "__main__":
     seed_users = list(pd.read_csv("data/seed_users.csv").user_id.values)
-     
+    print(type(seed_users[0])) 
     if sys.argv[1] == "seeds":
         print("Getting seeds followers")
         get_followers(seed_users, n_seeds = len(seed_users), oauth = oauth_seed, datetime = time.strftime("%y%m%d%H"))
@@ -63,22 +99,21 @@ if __name__ == "__main__":
     elif sys.argv[1] == "retweeters":
         print("Getting new retweeters followers")
         
-        while True:    
-            if 'collected_followers_ids.csv' in os.listdir('data/'):
-                with open('data/collected_followers_ids.csv') as file:
-                    collected_ids = file.read().splitlines()
-            else:
-                collected_ids = []
+        if 'collected_followers_ids.csv' in os.listdir('data/'):
+            with open('data/collected_followers_ids.csv') as file:
+                collected_ids = file.read().splitlines()
+        else:
+            collected_ids = []
  
-            all_retweeters = pd.read_csv("data/retweeters_users.csv")
-            print(len(all_retweeters))
-            # get only new retweeters
-            new_retweeters = all_retweeters[~all_retweeters.user_id.isin(collected_ids)].user_id.values
+        all_retweeters = pd.read_csv("data/retweeters_users.csv")
+        print(len(all_retweeters))
+        # get only new retweeters
+        new_retweeters = all_retweeters[~all_retweeters.user_id.isin(collected_ids)].user_id.values
             
-            if len(new_retweeters) == 0:
-                print('No more users to collect')
-                continue
-            
+        if len(new_retweeters) == 0:
+            print('No more users to collect')
+        
+	else:
             print('collecting %i users' %len(new_retweeters))
             n_group = int(sys.argv[2])
             oauth = oauths[n_group]
@@ -88,8 +123,4 @@ if __name__ == "__main__":
             get_followers(new_retweeters[n_group*n_per_group: (n_group+1)*n_per_group], n_seeds = 0,
                           oauth=oauth,
                           n_group = n_group)
-            
-            time.sleep(900) # Waiting rate limit time
-            
-            
             
