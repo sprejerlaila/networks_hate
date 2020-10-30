9a07e80ae4d3a4eedf26fbde9d00e21f14303431
Laila Sprejer <sprejerlaila@gmail.com>
2020-10-30 13:39:35 +0000

    create folder structure

---
+++ create_folder_structure.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 11:47:33 2020
+
+@author: lsprejer
+"""
+import os
+
+os.mkdir('data')
+
+os.mkdir('data/raw')
+os.mkdir('data/processed')
+
+os.mkdir('data/processed/seed_tweets')
+os.mkdir('data/processed/seed_retweets')
+os.mkdir('data/processed/seed_friends')
+os.mkdir('data/processed/seed_followers')
+os.mkdir('data/processed/retweeters_followers')
+
+os.mkdir('data/profiles')
+os.mkdir('data/media')
+os.mkdir('data/botometer')
